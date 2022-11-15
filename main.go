ac9cfb38a6d16d735d1781f219e69ca59e148399
Keiran Jensen <keiranjensen@gmail.com>
2022-11-14 19:59:30 -0500

    Implement simple GET, POST, and DELETE endpoints

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	router.GET("/GetEndpoint", func(c *gin.Context) {
+		c.String(http.StatusOK, "Placeholder response (Get)")
+	})
+
+	router.POST("/PostEndpoint", func(c *gin.Context) {
+		postData, _ := io.ReadAll(c.Request.Body)
+		c.String(http.StatusOK, (string)(postData)) // Placeholder response: return raw data received
+	})
+
+	router.DELETE("/DeleteEndpoint", func(c *gin.Context) {
+		c.String(http.StatusOK, "Placeholder response (Delete)")
+	})
+
+	router.Run()
 }
